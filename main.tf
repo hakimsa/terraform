661484395ecd71cbf27329eb988b74140d06e5cc
hakim <samouh591@gmail.com>
2025-03-09 03:05:01 +0100

    add Jenkinsfile

--- main.tf
+++ main.tf
@@ -2,26 +2,22 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0.2"
+      version = "3.0.2"
     }
   }
 }
 
 provider "docker" {
-
-
+  host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
+# Pulls the image
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.name
-  name  = "kim_nginx"
-
-  ports {
-    internal = 80
-    external = 8088
-  }
+# Create a container
+resource "docker_container" "foo" {
+  image = docker_image.ubuntu.image_id
+  name  = "foo"
 }
