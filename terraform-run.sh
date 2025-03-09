e5b3474a69141bc14dabbd7ecc1884125245e84e
hakim <samouh591@gmail.com>
2025-03-09 02:33:08 +0100

    add Jenkinsfile

--- terraform-run.sh
+++ terraform-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v $(pwd):/app -w /app terraform-container2 $@
-#docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock terraform-container $@
+#docker run --rm -v $(pwd):/app -w /app terraform-container2 $@
+docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock terraform-container2 $@
