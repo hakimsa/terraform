cfb83d9975e0723fb4facc356531a78894532c79
hakim <samouh591@gmail.com>
2025-03-09 02:24:40 +0100

    add Jenkinsfile

--- main.tf
+++ main.tf
@@ -2,13 +2,15 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.16.0"
+      version = "~> 3.0.2"
     }
   }
 }
 
 provider "docker" {
-  
+
+  host = "unix:///var/run/docker.sock"
+
 }
 
 resource "docker_image" "nginx" {
